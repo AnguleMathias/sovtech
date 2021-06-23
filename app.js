8e967bd0bff4e8f7165c5fd6ab54b3ffbe4cd6e1
AnguleMathias <angulemathias3@gmail.com>
2021-06-23 09:53:10 +0300

    person object type description

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(
+  graphqlHTTP({
+    graphiql: true,
+    pretty: true,
+  })
+);
 
 const port = process.env.PORT || 5000;
 
